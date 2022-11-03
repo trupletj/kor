e227670fbda58005db28788c328ed3952fcfabcb
trupletj <tuvshinjargal01@gmail.com>
2022-11-03 18:27:14 +0800

    private github

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import Logo from "../components/Logo";
+
+import Search from "../assets/Search";
+import Logo from "../assets/Logo";
+import Settings from "../assets/Settings";
+import BurgerMenu from "../assets/BurgerMenu";
 
 const Home = () => {
   return (
@@ -11,7 +15,10 @@ const Home = () => {
                     text-white text-2xl
 "
       >
+        <BurgerMenu />
         <Logo />
+        <Search />
+        <Settings />
       </header>
       <footer
         className="w-full h-16 bg-[#1B1C1E]
