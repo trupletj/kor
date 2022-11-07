9767c577579aaa1f54fbebcaa4c0194ce6e28653
trupletj <tuvshinjargal01@gmail.com>
2022-11-08 00:02:46 +0800

    1

--- src/container/Integration.jsx
+++ src/container/Integration.jsx
@@ -138,6 +138,75 @@ const Integration = () => {
                 </div>
               </div>
             </li>
+            <li>
+              <div className="flex w-full text-sm mb-4">
+                <div className="w-24 h-24 aspect-square overflow-hidden">
+                  <img
+                    src="https://picsum.photos/200/300?random=5"
+                    className="object-cover h-24 w-24 "
+                  />
+                </div>
+                <div className="flex flex-col justify-between  pl-2">
+                  <p className="font-bold">영유아 원목 장난감</p>
+                  <span>
+                    <p>{"어린이제품 안전특별법 > 안전확인"}</p>
+                  </span>
+                  <p>제조업체명 : Ningbo</p>
+                  <p className="">
+                    문서구분 :{" "}
+                    <span className=" text-[#008AFF]">
+                      인증서 (CB064R1114-2001)
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="flex w-full text-sm mb-4">
+                <div className="w-24 h-24 aspect-square overflow-hidden">
+                  <img
+                    src="https://picsum.photos/200/300?random=5"
+                    className="object-cover h-24 w-24 "
+                  />
+                </div>
+                <div className="flex flex-col justify-between  pl-2">
+                  <p className="font-bold">영유아 원목 장난감</p>
+                  <span>
+                    <p>{"어린이제품 안전특별법 > 안전확인"}</p>
+                  </span>
+                  <p>제조업체명 : Ningbo</p>
+                  <p className="">
+                    문서구분 :{" "}
+                    <span className=" text-[#008AFF]">
+                      인증서 (CB064R1114-2001)
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="flex w-full text-sm mb-4">
+                <div className="w-24 h-24 aspect-square overflow-hidden">
+                  <img
+                    src="https://picsum.photos/200/300?random=5"
+                    className="object-cover h-24 w-24 "
+                  />
+                </div>
+                <div className="flex flex-col justify-between  pl-2">
+                  <p className="font-bold">영유아 원목 장난감</p>
+                  <span>
+                    <p>{"어린이제품 안전특별법 > 안전확인"}</p>
+                  </span>
+                  <p>제조업체명 : Ningbo</p>
+                  <p className="">
+                    문서구분 :{" "}
+                    <span className=" text-[#008AFF]">
+                      인증서 (CB064R1114-2001)
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </li>
           </ul>
         </section>
       </Container>
